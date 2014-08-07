b1c23adab3b508a48160bd9523b6f40e894e215f
Robin Ward <robin.ward@gmail.com>
2014-08-06 23:28:08 -0400

    Easier to debug

--- app/controllers/bugs/index.js
+++ app/controllers/bugs/index.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+
   // Calculate the average bug size
   averageSize: function() {
+
     var total = 0;
-    this.get('model').forEach(function(b) {
+    var bugs = this.get('model');
+
+    bugs.forEach(function(b) {
       total += b.get('size');
     });
+
     return Math.round(total / this.get('length'));
+
   }.property('@each.size')
+
 });
